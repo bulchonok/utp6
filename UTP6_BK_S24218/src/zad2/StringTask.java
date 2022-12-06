708ce3b17b2393add6358a2dbe472b7490ac2501
Kiryl <102504342+bulchonok@users.noreply.github.com>
2022-12-06 15:06:35 +0100

    Add files via upload

--- UTP6_BK_S24218/src/zad2/StringTask.java
+++ UTP6_BK_S24218/src/zad2/StringTask.java
@@ -1,4 +1,32 @@
 package zad2;
 
-public class StringTask {
+public class StringTask implements Runnable{
+    public StringTask(String str, int i){
+
+    }
+    public void start(){
+        
+    }
+    public void abort(){
+        
+    }
+    public boolean isDone() {
+
+    }
+
+    @Override
+    public void run() {
+        
+    }
+
+    public String getResult() {
+    }
+
+    enum state {RUNNING,ABORTED,READY}
+    public state getState() {
+
+        return state.RUNNING;
+        return state.ABORTED;
+        return state.READY;
+    }
 }
